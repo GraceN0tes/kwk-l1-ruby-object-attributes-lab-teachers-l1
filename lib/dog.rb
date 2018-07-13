cdb43d1aebab29060794ab16f8b4221953412c23
Grace Park <parkgra3@gmail.com>
2018-07-13 18:52:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # dog.rb
+
+class dog
+  
+ def name=(name)
+  @name = name
+end
+
+def name
+  @name
+end
+
+def breed =(breed)
+  @breed = breed
+end
+
+def breed
+  @breed
+end
+
+
+end
+  
+end
